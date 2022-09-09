bced56888446f79bcab758a134f4541968015f14
T-GAMER <gabrielgondim5609@gmail.com>
2022-09-08 22:57:08 -0300

    Primeiro commit

---
+++ trab-operacoes-lfa/main.py
@@ -0,0 +1,38 @@
+from AutomatoFD import AutomatoFD
+
+if __name__ == '__main__':
+
+    afd = AutomatoFD('ab')
+
+    for i in range(1, 9):
+        afd.criaEstado(i)
+    afd.mudaEstadoInicial(1)
+    afd.mudaEstadoFinal(4, True)
+
+    afd.criaTransicao(1, 5, 'a')
+    afd.criaTransicao(2, 7, 'a')
+    afd.criaTransicao(3, 5, 'a')
+    afd.criaTransicao(4, 8, 'a')
+    afd.criaTransicao(5, 1, 'a')
+    afd.criaTransicao(6, 3, 'a')
+    afd.criaTransicao(7, 1, 'a')
+    afd.criaTransicao(8, 4, 'a')
+    afd.criaTransicao(1, 2, 'b')
+    afd.criaTransicao(2, 2, 'b')
+    afd.criaTransicao(3, 4, 'b')
+    afd.criaTransicao(4, 4, 'b')
+    afd.criaTransicao(5, 6, 'b')
+    afd.criaTransicao(6, 6, 'b')
+    afd.criaTransicao(7, 8, 'b')
+    afd.criaTransicao(8, 8, 'b')
+
+
+    print(afd)
+
+    cadeia = 'abbabaabbbbbba'
+    afd.limpaAfd()
+    parada = afd.move(cadeia)
+    if not afd.deuErro() and afd.estadoFinal(parada):
+        print('Aceita cadeia "{}"'.format(cadeia))
+    else:
+        print('Rejeita cadeia "{}"'.format(cadeia))
